f1c0ff8b916943b3b3fd563a386d9b910d665263
KanHar <knaan.harpaz@gmail.com>
2023-08-13 17:43:02 +0300

    Update CI

--- lint_and_test.sh
+++ lint_and_test.sh
@@ -1,4 +1,4 @@
-python -m poetry run black connectedpapers tests usage_samples
+python -m poetry run black .
 python -m poetry run isort .
 python -m poetry run mypy --strict connectedpapers tests usage_samples
 python -m poetry run flake8 connectedpapers tests usage_samples
